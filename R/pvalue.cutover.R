95f09943f0d814816c9191883240585d52b2f987
ROBERT GRAHAM CLARK <robertgrahamclark@gmail.com>
2016-06-01 14:26:27 +1000

    tuning parameter eps added to pvalue.cutover

--- R/pvalue.cutover.R
+++ R/pvalue.cutover.R
@@ -15,6 +15,9 @@
 #' @param cutover0 The hypothesized value of the cutover at which the link
 #' function switches smoothly from link1 to link2. Must be strictly greater 
 #' than 0 and strictly less than 1.
+#' @param eps A tuning parameter. The MLE of the cutover is restricted to the range
+#' eps to (1-eps). Usually eps=0 is appropriate, but occasionally a small positive value
+#' may be needed to avoid numerical problems in the maximisation of the likelihood.
 #' @return The p-value of a test of the null hypothesis that cutover=cutover0.
 #' 
 #' @import stats
@@ -22,12 +25,12 @@
 #' pvalue.cutover(y~x1+x2 , data=loglogit.example,link1="log",link2="logit",
 #' cutover0=0.8)
 #' @export
-pvalue.cutover <- function(data,formula,link1,link2,cutover0){
+pvalue.cutover <- function(data,formula,link1,link2,cutover0,eps=0){
   deviance.cutover <- function(cutover){
     glm(formula=formula,data=data,
          family=binomial(blendedLink(link1,link2,cutover)))$deviance
   }
-  eps <- 0.01
+  eps <- 0.025
   deviance.optcutover <- optimize(f=deviance.cutover,interval=c(eps,1-eps))$objective
   deviance.cutover0 <- glm(formula=formula,data=data,
                            family=binomial(blendedLink(link1,link2,cutover0)))$deviance
